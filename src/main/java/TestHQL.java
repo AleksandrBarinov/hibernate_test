5ba2284bd34d27ab9df8cf5e8abd29a880a2726f
AleksandrBarinov <nosprut@gmail.com>
2019-07-07 16:51:14 +0300

    order by

--- src/main/java/TestHQL.java
+++ src/main/java/TestHQL.java
@@ -11,19 +11,19 @@ public class TestHQL {
         session.beginTransaction();
 
 
-        Query query = session.createQuery("select distinct c.number from Car c where id = 11 or number = :n");
+        Query query = session.createQuery("from Car c where id = 11 or number = :n order by number desc ");
         query.setParameter("n","400");
+
+//        query.setMaxResults(2);
+//        query.setFirstResult(1);
 
 //        Car car = (Car) query.uniqueResult();
 //        System.out.println(car.getNumber());
 
-        List<String> stringList = query.list();
-        System.out.println(stringList.size());
-
-//        List<Car> list = query.list();
-//        for (Car car: list){
-//            System.out.println(car.getNumber());
-//        }
+        List<Car> list = query.list();
+        for (Car car: list){
+            System.out.println(car.getNumber());
+        }
 
         session.getTransaction().commit();
     }
