d7b27e5e697a0b19f80ed97bb084f973c2c34e77
AleksandrBarinov <nosprut@gmail.com>
2019-07-07 16:57:46 +0300

    group by having

--- src/main/java/TestHQL.java
+++ src/main/java/TestHQL.java
@@ -11,19 +11,24 @@ public class TestHQL {
         session.beginTransaction();
 
 
-        Query query = session.createQuery("from Car c where id = 11 or number = :n order by number desc ");
+        Query query = session.createQuery("select c.number from Car as c where id = 11 or number = :n group by c.number having c.number = :n");
         query.setParameter("n","400");
 
 //        query.setMaxResults(2);
 //        query.setFirstResult(1);
 
+        List<String> groupedNums = query.list();
+        for (String s: groupedNums){
+            System.out.println(s);
+        }
+
 //        Car car = (Car) query.uniqueResult();
 //        System.out.println(car.getNumber());
 
-        List<Car> list = query.list();
-        for (Car car: list){
-            System.out.println(car.getNumber());
-        }
+//        List<Car> list = query.list();
+//        for (Car car: list){
+//            System.out.println(car.getNumber());
+//        }
 
         session.getTransaction().commit();
     }
